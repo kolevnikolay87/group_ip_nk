46ec40178a9941f2ad2ded2deee75c4b17ea9e3e
Nikolay <nikolay_kolev@flutterint.com>
2023-06-02 12:02:17 +0300

    m

--- resources.tf
+++ resources.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 resource "aws_codebuild_project" "cloud_nuke_project" {
   name          = "CloudNukeProject"
   description   = "CodeBuild project to execute CloudNuke"
@@ -25,3 +26,18 @@ resource "aws_codebuild_project" "cloud_nuke_project" {
     report_build_status = false
   }
 }
+=======
+resource "aws_cloudwatch_event_rule" "codebuild" {
+  name        = "trigger-eventbridge-codebuild"
+  description = "Trigger a codebiuld"
+  account = "790184663615"
+  role_arn = aws_iam_role.eventbridge_codebuild_role.arn
+
+  event_pattern = jsonencode({
+    detail-type = [
+      "AWS Console Sign In via CloudTrail"
+    ]
+  })
+  schedule_expression = var.schedule_expression
+}
+>>>>>>> e6ad702 (adjusted iam.tf, added schedule_expression variable and adjusted role + attachment)
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 variable "aws_region" {
   description = "AWS region where the resources will be created."
   type        = string
@@ -23,3 +24,10 @@ variable "type_of_artifacts" {
   type = string
   default = "NO_ARTIFACTS"
 }
+=======
+variable "schedule_expression" {
+  type = string
+  description = "cronjob schedule expression"
+  default = "cron(0 20 * * ? *)"
+}
+>>>>>>> e6ad702 (adjusted iam.tf, added schedule_expression variable and adjusted role + attachment)
